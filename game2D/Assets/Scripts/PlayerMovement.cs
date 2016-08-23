e7b11c364ee5dd33e1fed7863b86edb6c4ec108a
MMEstrada <MMEstrada90@gmail.com>
2016-08-22 19:52:48 -0700

    New repo

---
+++ game2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 50f;
+    public float maxSpeed = 3f;
+    public float jumpHeight;
+    
+    public bool facingRight = true;
+    public bool canJump = true;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+	
+	}
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (h * rb2d.velocity.x < maxSpeed)
+        {
+            rb2d.AddForce(Vector2.right * h * moveSpeed);
+        }
+
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+        }
+
+        if (h > 0 && !facingRight)
+        {
+            Flip();
+        }
+
+        if (h < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            rb2d.AddForce(Vector2.up * jumpHeight);
+            canJump = false;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "ground")
+        {
+            canJump = true;
+        }
+    }
+}
---
+++ game2D/Assets/Scripts/UIFunctions.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class UIFunctions : MonoBehaviour {
+
+    public GameObject menus;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void New_Game()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Load_Game()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Exit_Game()
+    {
+        Application.Quit();
+    }
+
+    public void Start_Game()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
